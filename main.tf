cab090e99b25cc6083756da7f6485a616a715d11
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2023-05-04 17:41:57 +0100

    Update module to use values file

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+
 resource "helm_release" "aws_ebs" {
   name       = "aws-ebs-csi-driver"
   chart      = "aws-ebs-csi-driver"
@@ -5,27 +6,6 @@ resource "helm_release" "aws_ebs" {
   namespace  = "kube-system"
   version    = "2.10.1"
 
-  set {
-    name  = "controller.serviceAccount.create"
-    value = "false"
-  }
-
-  set {
-    name  = "controller.tolerations[0].key"
-    value = "monitoring-node"
-  }
-  set {
-    name  = "controller.tolerations[0].value"
-    value = "true"
-  }
-  set {
-    name  = "controller.tolerations[0].operator"
-    value = "Equal"
-  }
-  set {
-    name  = "controller.tolerations[0].effect"
-    value = "NoSchedule"
-  }
-
-  depends_on = [module.ebs_irsa]
+  values = [templatefile("${path.module}/templates/values.yaml.tpl", {
+  })]
 }
