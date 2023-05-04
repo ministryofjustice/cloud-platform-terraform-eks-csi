5058770be347017cb39b69da929bcd5820da423c
Vijay Veeranki <vijay.veeranki@digital.justice.gov.uk>
2023-05-04 17:18:47 +0100

    Remove vars related to dynamic

--- variables.tf
+++ variables.tf
@@ -2,16 +2,3 @@ variable "eks_cluster" {
   type        = string
   description = "cluster name"
 }
-
-variable "tolerations" {
-  type = list(map(string))
-  default = [
-    {
-      key      = "monitoring-node",
-      operator = "Equal",
-      value    = "true",
-      effect   = "NoSchedule"
-    }
-  ]
-  description = "Tolerations to apply to deployment"
-}
